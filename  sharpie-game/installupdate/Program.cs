640f51b51da92a5adccaca5586905b9836d064a1
adirucio96@gmail.com <adirucio96@gmail.com@89e43c49-a76e-0d6c-0fb0-9ffc3f569227>
2012-06-25 18:36:06 +0000

    Poprawiony installupdater, "bardziej myśli" za użyszkodnika.

---  sharpie-game/installupdate/Program.cs
+++  sharpie-game/installupdate/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Diagnostics;
 using System.IO;
+using System.Windows.Forms;
+using System.Threading;
 
 namespace installupdate
 {
@@ -22,31 +24,69 @@ namespace installupdate
         static void Main(string[] args)
         {
             string[] files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.updt");
+            Application.EnableVisualStyles();
 
             if (args.Length == 0)
             {
-                throw new Exception("Brak argumentów! Co ja mam zamknąć?");
-            }
-            Process proc = GetaProcess(args[0]);
-            if (proc != null)
-            {
-                proc.CloseMainWindow();
-                proc.WaitForExit();
-                proc.Close();
+                Console.WriteLine("Brak argumentów! Co ja mam instalować?\nNaciśnij dowolny klawisz, aby kontynuować ...");
+                Console.ReadKey();
+                Environment.Exit(0);
             }
 
+            FindAndKillProcess(Path.GetFileNameWithoutExtension(args[0]));
+            MessageBox.Show("Upewnij się, że wszystkie okna gry/edytora są zamknięte!", "Ostrzeżenie", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            bool retry = false;
             foreach (string s in files)
             {
-                Console.WriteLine(s);
-                Console.WriteLine(Path.GetFileNameWithoutExtension(s));
-                if (File.Exists(Path.GetFileNameWithoutExtension(s)))
+                do
                 {
-                    File.Delete(Path.GetFileNameWithoutExtension(s));
-                }
-                File.Move(s, Path.GetFileNameWithoutExtension(s));
+                    try
+                    {
+                        if (Path.GetExtension(s) == ".updt")
+                        {
+                            if (File.Exists(Path.GetFileNameWithoutExtension(s)))
+                            {
+                                File.Delete(Path.GetFileNameWithoutExtension(s));
+                            }
+                            File.Move(s, Path.GetFileNameWithoutExtension(s));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        DialogResult res = MessageBox.Show("Nie można uzyskać dostępu do pliku " + Path.GetFileNameWithoutExtension(s) + ".\nUpewnij się, że plik jest zamknięty i spróbuj jeszcze raz!\n\nPełna treść błędu:\n" + ex.Message, "Błąd", MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning);
+                        if (res == DialogResult.Retry)
+                        {
+                            retry = true;
+                        }
+                        else
+                        {
+                            DialogResult res2 = MessageBox.Show("Na pewno chcesz przerwać instalację aktualizacji?\nByć może nie będziesz w stanie uruchomić gry/edytora!", "Ostrzeżenie", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+                            if (res2 == DialogResult.Yes)
+                            {
+                                Environment.Exit(0);
+                            }
+                            else
+                            {
+                                retry = true;
+                            }
+                        }
+                    }
+                } while (retry);
             }
 
-            if (proc != null) { Process.Start(Path.GetFullPath(args[0])); }
+            Process.Start(Path.GetFullPath(args[0]));
+        }
+
+        public static void FindAndKillProcess(string name)
+        {
+            foreach (Process clsProcess in Process.GetProcesses())
+            {
+                if (clsProcess.ProcessName.StartsWith(name))
+                {
+                    clsProcess.Kill();
+                }
+            }
         }
     }
 }
